d91c9de2ce154f0d3ec38328aea496ef568eca16
rdivekar123 <rdivekar@umich.edu>
2022-01-17 09:31:18 -0800

    add functions : yaw and curvature ref generation

--- kinematic_lqr_demo.m
+++ kinematic_lqr_demo.m
@@ -23,29 +23,16 @@ x_spline = csaps(1:1:length(path),path(:,1),0.1,1:0.01:length(path));
 y_spline = csaps(1:1:length(path),path(:,2),0.1,1:0.01:length(path));
 % f = figure()
 % plot(x_spline, y_spline)
-yaw_spline = zeros(1, length(x_spline));
-for i = 2:length(x_spline)-1
-    x_next = x_spline(i+1);
-    x_prev = x_spline(i-1);
-    y_next = y_spline(i+1);
-    y_prev = y_spline(i-1);
-    yaw_spline(i) = atan2(y_next-y_prev, x_next-x_prev);
-end
-yaw_spline(1) = yaw_spline(2);
-yaw_spline(end) = yaw_spline(end-1);
+
+% calculate yawref
+yaw_spline = calculateYawref(x_spline, y_spline);
+
 xy_spline = [x_spline', y_spline'];
 
 % insert Meneger curvature
-curvature_spline = zeros(1, length(x_spline));
-for i = 2:length(x_spline)-1
-    point1 = xy_spline(i-1,:);
-    point2 = xy_spline(i,:);
-    point3 = xy_spline(i+1,:);
-    area = ((point2(1)-point1(1))*(point3(2)-point1(2)) - (point2(2)-point1(2))*(point3(1)-point1(1)))/2;
-    curvature_spline(i) = 4 * area / (norm(point1-point2) * norm(point2-point3) * norm(point3-point1));
-end
-curvature_spline(1) = curvature_spline(2);
-curvature_spline(end) = curvature_spline(end-1);
+curvature_spline = calculateCurvature(xy_spline);
+
+% insert velocity
 v_spline = 15.0 *ones(1, length(x_spline));
 
 ref_line = [x_spline', y_spline', yaw_spline', v_spline', curvature_spline'];
@@ -74,5 +61,30 @@ legend('reference path','ego path')
 grid on 
 hold off;
 
+function yaw_ref = calculateYawref(x,y)
+% calculateYawref Local function calculates yaw angles from given x, y path.
+yaw_ref = zeros(1, length(x));
+for i = 2:length(x)-1
+    x_next = x(i+1);
+    x_prev = x(i-1);
+    y_next = y(i+1);
+    y_prev = y(i-1);
+    yaw_ref(i) = atan2(y_next-y_prev, x_next-x_prev);
+end
+yaw_ref(1) = yaw_ref(2);
+yaw_ref(end) = yaw_ref(end-1);
+end
 
-
+function curvature_ref = calculateCurvature(xy_path)
+% calculateYawref Local function calculates yaw angles from given xy path.
+curvature_ref = zeros(1, length(xy_path));
+for i = 2:length(xy_path)-1
+    point1 = xy_path(i-1,:);
+    point2 = xy_path(i,:);
+    point3 = xy_path(i+1,:);
+    area = ((point2(1)-point1(1))*(point3(2)-point1(2)) - (point2(2)-point1(2))*(point3(1)-point1(1)))/2;
+    curvature_ref(i) = 4 * area / (norm(point1-point2) * norm(point2-point3) * norm(point3-point1));
+end
+curvature_ref(1) = curvature_ref(2);
+curvature_ref(end) = curvature_ref(end-1);
+end
